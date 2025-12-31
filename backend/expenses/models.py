8abddbd0a7a8fe317e5d696a8b4692e816f2e911
Judith-AB <142833346+Judith-AB@users.noreply.github.com>
2025-12-31 18:36:23 +0530

    Added Expense model and admin registration

--- backend/expenses/models.py
+++ backend/expenses/models.py
@@ -1,6 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 class Expense(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,null=True,blank=True)
     amount = models.FloatField()
     description = models.TextField()
     category = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.amount}"
