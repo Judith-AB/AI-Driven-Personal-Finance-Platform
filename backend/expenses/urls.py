4e42d828ea26c0bc0b78c9886e4b15f436a02a77
Judith-AB <142833346+Judith-AB@users.noreply.github.com>
2026-01-06 22:04:53 +0530

    Added budget status API

--- backend/expenses/urls.py
+++ backend/expenses/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import ExpenseAPIView,BudgetAPIView
+from .views import ExpenseAPIView,BudgetAPIView,BudgetStatusAPIView
 urlpatterns=[path('expenses/',ExpenseAPIView.as_view(),name='expenses'),
              path('budgets/',BudgetAPIView.as_view(),name="budgets"),
+             path("budget-status/",BudgetStatusAPIView.as_view(),name="budget-status")
              ]
--- backend/expenses/views.py
+++ backend/expenses/views.py
@@ -3,9 +3,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-
 from .models import Expense,Budget
 from .serializers import ExpenseSerializer,BudgetSerializer
+
+from datetime import datetime
+from django.db.models import Sum
 
 class ExpenseAPIView(APIView):
     def get(self,request):
@@ -31,3 +33,29 @@ class BudgetAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class BudgetStatusAPIView(APIView):
+    def get(self,request):
+        now=datetime.now()
+        month=now.month
+        year=now.year
+        total_spent=(Expense.objects.filter(created_at__month=month,created_at__year=year)
+                     .aggregate(total=Sum("amount"))["total"]
+                     ) or 0
+        budget=Budget.objects.filter(month=month,year=year).first()
+        if budget:
+            remaining_budget=budget.amount-total_spent
+            budget_exceeded=total_spent>budget.amount
+            budget_amount=budget.amount
+        else:
+            remaining_budget=None
+            budget_exceeded=False
+            budget_amount=None
+        return Response({
+            'month':month,
+            'year':year,
+            'total_spent':total_spent,
+            'budget':budget_amount,
+            'remaining_budget':remaining_budget,
+            'budget_exceeded':budget_exceeded
+                                                    })
