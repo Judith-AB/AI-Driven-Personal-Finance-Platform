a53d0c8da8859138712b735ffb167cbd6743e66b
Judith-AB <142833346+Judith-AB@users.noreply.github.com>
2026-01-11 22:24:22 +0530

    Modified the expense class and set default category as Others

--- backend/expenses/serializers.py
+++ backend/expenses/serializers.py
@@ -4,7 +4,8 @@ from .models import Expense,Budget
 class ExpenseSerializer(serializers.ModelSerializer):
     class Meta:
         model=Expense
-        fields='__all__'
+        fields = ['id', 'amount', 'description', 'category', 'created_at', 'user']
+        read_only_fields = ['category', 'user']
 class BudgetSerializer(serializers.ModelSerializer):
     user=serializers.ReadOnlyField(source='user.id')
     class Meta:
--- backend/expenses/views.py
+++ backend/expenses/views.py
@@ -17,7 +17,7 @@ class ExpenseAPIView(APIView):
     def post(self,request):
         serializer=ExpenseSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save(user=request.user)
+            serializer.save(user=request.user,category='Others')
             return Response(serializer.data,status=status.HTTP_201_CREATED)
         return Response(serializer.data,status=status.HTTP_400_BAD_REQUEST)
     
